babe7a874f145bb04fe8aa898aaf1a83aa4794e1
anwar110207-oss <anwar110207@gmail.com>
2026-09-09 09:55:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,43 @@
 window.onload = function () {
     alert("🎟️ Welcome to anwar110207-oss Ticket Booking!");
 };
+function makeRed() {
+    document.getElementById("home").style.color = "red";
+}
 
+function makeBlue() {
+    document.getElementById("home").style.color = "blue";
+}
+
+function makeGreen() {
+    document.getElementById("home").style.color = "green";
+}
+
+
+function yellowBackground() {
+    document.body.style.backgroundColor = "yellow";
+}
+
+function blueBackground() {
+    document.body.style.backgroundColor = "lightblue";
+}
+
+function whiteBackground() {
+    document.body.style.backgroundColor = "white";
+}
+
+function darkMode() {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+}
+
+function lightMode() {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+}
+
+function changeMessage() {
+    document.getElementById("messages").innerHTML = "Thank you for visiting our ticketing website!";
 // Booking Form
 const bookingForm = document.getElementById("bookingForm");
 
